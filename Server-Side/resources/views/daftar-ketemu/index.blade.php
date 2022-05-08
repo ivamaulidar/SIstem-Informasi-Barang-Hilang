68e2ba593a8b82653db9aa81f2962d71eab11ebf
HafizhL15 <muhhafizh33@gmail.com>
2022-05-09 01:10:26 +0700

    Update Tampilan Barang

--- Server-Side/resources/views/daftar-ketemu/index.blade.php
+++ Server-Side/resources/views/daftar-ketemu/index.blade.php
@@ -1,78 +1,77 @@
-@extends('layouts.main2')
-@section('title','Daftar Penemuan Barang')
 
-
-@section('content')
-    <h1 class="h3 mb-4 text-gray-800" style="text-align: center">Daftar Penemuan Barang</h1><br>
-    <div class="d-flex justify-content-center">
-        @foreach ($penemuan as $item)
-            <div class="card mb-3" style="max-width: 540px">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                    <img src="{{ asset('/asset/img2/'.$item->gambar) }}" class="img-fluid rounded-start" >
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Barang Temuan</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-widht, initial-scale=1">
+        <link rel="shortcut icon" href="img/logo.svg">
+        <link rel="stylesheet" href="css/baranghilang.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <nav>
+                <img class="logo" src="img/logo.svg" alt="logo">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/daftar_barang_hilang">Kehilangan</a></li>
+                    <li><a href="/daftar_barang_ketemu">Penemuan</a></li>
+                    <li><a href="#">Isi Laporan <ion-icon name="chevron-down"></ion-icon></a>
+                        <ul>
+                            <li><a href="/form_kehilangan">Laporan Kehilangan</a></li>
+                            <li><a href="/form_penemuan">Laporan Penemuan</a></li>
+                        </ul>
+                    </li>
+                    @can('admin')
+                    <li><a href="/admin">Admin</a></li>                        
+                    @endcan
+                </ul>
+                <form action="{{ route('logout') }}" method="POST" >
+                    @csrf
+                        {{-- <button type="submit" {{ __('Login') }}>Keluar</button> --}}
+                    <button type="submit" class="button" {{ __('Login') }}>
+                        <span class="icon">
+                            <ion-icon name="exit-outline"></ion-icon>
+                        </span>
+                        <span class="text">Keluar</span>
+                    </button>
+                </form>
+            </nav>
+        </div>
+        <h1>Daftar Barang Temuan</h1>
+        <div class="container">
+            @foreach ($penemuan as $item)
+                <div class="card">
+                    <div class="card-header">
+                        <img src="{{ asset('/asset/img/'.$item->gambar) }}">
                     </div>
-                    <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">Jenis Barang {{ $item->jenis }}</h5>
-                        <p class="card-text">Tanggal Hilang {{ $item->tanggal_ketemu }}</p>
-                        <p class="card-text">Spesifikasi : {{ $item->spek }}</p>
-                        <p class="card-text"><small class="text-muted">{{ $item->kronologi }}</small></p>
-                    </div>
+                    <table>
+                        <tr>
+                            <td>Tanggal Hilang:</td>
+                            <td>{{ $item->tanggal }}</td>
+                        </tr>
+                        <tr>
+                            <td>Jenis Barang:</td>
+                            <td>{{ $item->jenis }}</td>
+                        </tr>
+                        <tr>
+                            <td>Spesifikasi:</td>
+                            <td>{{ $item->spek }}</td>
+                        </tr>
+                        <tr>
+                            <td>Kronologi:</td>
+                            <td>{{ $item->kronologi }}</td>
+                        </tr>
+                    </table>
                     </div>
                 </div>
-            </div>
-        @endforeach
-        
-    </div>
-    
-{{-- 
-    @php
-        $increment = 1;
-    @endphp
-    @foreach ($kehilangan as $item)
-        <tr>
-            <td>{{ $increment++ }}</td>
-            <td><img src="{{ asset('/asset/img/'.$item->gambar) }}" style="width: 70px; height:70px"></td>
-            <td>{{ $item->nama }}</td>
-            <td>{{ $item->nim }}</td>
-            <td>{{ $item->prodi }}</td>
-            <td>{{ $item->angkatan }}</td>
-            <td>{{ $item->tanggal }}</td>
-            <td>{{ $item->jenis }}</td>
-            <td>{{ $item->spek }}</td>
-            <td>{{ $item->kronologi }}</td>
-            <td>  
-                <a href="{{ route('kehilangan.edit', $item->id) }}" class="btn btn-warning"><i class="fa fa-pencil-alt"></i></a><br>
-                <form action="{{ route('kehilangan.destroy', $item->id) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" onclick="return confirm('Anda yakin ingin menghapus data tersebut ?')" class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                </form>
-            </td>
-        </tr>
-    @endforeach --}}
-
-
-    {{-- <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $increment = 1;
-            @endphp
-            @foreach ($users as $item)
-                <tr>
-                    <td>{{ $increment++ }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->email }}</td>
-                </tr>
             @endforeach
-        </tbody>
-    </table> --}}
-@endsection
+            
+        </div>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    </body>
+</html>
 
